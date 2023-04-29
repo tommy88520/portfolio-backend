aff4af9387a8136a154a5620deee11e39f794073
Tommy <86784639+tommy88520@users.noreply.github.com>
2023-04-29 20:54:45 +0800

    project init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	// same as
+	// config := cors.DefaultConfig()
+	// config.AllowAllOrigins = true
+	// router.Use(cors.New(config))
+	router.Use(cors.Default())
+	router.Run()
+}
